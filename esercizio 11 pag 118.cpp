516ad619f992b20eb25a213b7de4616bd61c9d04
Alessandro Ramirez <ramirezmanrique.rogersjoaquinalessandro.2023@itisgalileiroma.it>
2025-12-04 09:01:37 +0100

    Add files via upload

---
+++ esercizio 11 pag 118.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+    double a, b;
+    cout << "Inserisci il coefficiente a: ";
+    cin>>a;
+
+    cout << "Inserisci il coefficiente b: ";
+    cin>>b;
+
+    if(a == 0){
+        if (b == 0){
+            cout << "L'equazione ha infinete soluzioni." << endl;
+        }else{
+            cout << "L'equazione non ha soluzioni." << endl;
+        }
+    }else{
+        double x= -b/a;
+        cout << "La soluzione dell'equazione è: x= " <<x<<endl;
+    }
+    return 0;
+}
